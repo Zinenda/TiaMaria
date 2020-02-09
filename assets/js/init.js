3bd5d334d74e7b4d8afe30b808c499626bbaf29b
Ivo Zinenda <izinenda@gmail.com>
2020-02-09 21:57:25 +0200

    new header

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
+    $('.slider').slider({
+        indicators: false
+    });
     $('.tabs').tabs();
     $('.tooltipped').tooltip();
     $('.fixed-action-btn').floatingActionButton();
